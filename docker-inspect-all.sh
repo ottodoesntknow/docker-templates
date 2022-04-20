d4e0b3db1bbc58cf5d933d61956e9753f9da0583
σттσ ∂σєѕη'т кησω <agoscar2001@gmail.com>
2022-04-20 17:23:24 +0200

    strip leading dash

--- docker-inspect-all.sh
+++ docker-inspect-all.sh
@@ -2,6 +2,6 @@
 
 # bash scipt that runs docker inspect on all containers and saved the output to docker-inspect-container_name.txt
 for container in $(docker ps -a -q); do
-    container_name=$(docker inspect --format="{{.Name}}" $container)
+    container_name=$(docker inspect --format="{{.Name}}" $container | sed 's/^\///')
     docker inspect $container > docker-inspect-$container_name.txt
 done
